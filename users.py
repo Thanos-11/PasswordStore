cdcd163997ef93c6def8b063b195c36dec568400
Ted Mwangi <tedletheorde@gmail.com>
2020-09-05 08:53:45 +0300

    created instance variables to take up user data

--- users.py
+++ users.py
@@ -4,4 +4,11 @@ class User:
     """
 
     user_list=[]
-    
+
+ # created instance variables to take up email,name,username and password of user
+ 
+    def __init__(self,email,name,username,password):
+        self.email=email
+        self.name=name
+        self.username=username
+        self.password=password
