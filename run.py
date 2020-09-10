32151bbc9703278b2a8b561fd2831bf6fe917b30
Ted Mwangi <tedletheorde@gmail.com>
2020-09-10 18:03:26 +0300

    Added few basic functions

--- run.py
+++ run.py
@@ -9,3 +9,29 @@ def create_login(email,name,username,password):
     new_user=User(email,name,username,password)
     return new_user
 
+def save_login(user):
+    """
+    Function to save user login details
+    """
+    user.save_login()
+
+def create_existing_credentials(social_app_name ,app_username , app_password):
+    """
+    function to create existing credentials accounts 
+    """
+    new_credential=Credentials(social_app_name ,app_username , app_password)
+    return new_credential
+
+def save_credentials(credential):
+    """
+    function to save credentials
+    """
+    credential.save_credentials()
+
+def delete_credential(credential):
+    """
+    function to delete credentials
+    """
+    credential.delete_credential()
+
+
